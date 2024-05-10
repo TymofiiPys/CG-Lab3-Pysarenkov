ca6daa316c95dc493d0d02397420f16fb8a75698
TymofiiPys <71066289+TymofiiPys@users.noreply.github.com>
2024-05-10 20:07:28 +0300

    Change EventPointComparator to compare events instead of points

--- src/main/java/org/cglab3/BentleyOttmann/EventPointComparator.java
+++ src/main/java/org/cglab3/BentleyOttmann/EventPointComparator.java
@@ -8,14 +8,14 @@ import java.util.Comparator;
  * if y coordinates are equal then if x coordinate point 1 is larger than of point 2, point 1 is larger.
  * If point is larger, then the event the point is associated with happens later during sweeping
  */
-public class EventPointComparator implements Comparator<Point2D> {
+class EventPointComparator implements Comparator<Event> {
 
     @Override
-    public int compare(Point2D o1, Point2D o2) {
-        if(o1.getY() < o2.getY())
+    public int compare(Event o1, Event o2) {
+        if(o1.getAssociatedPoint().getY() < o2.getAssociatedPoint().getY())
             return 1;
-        if(o1.getY() > o2.getY())
+        if(o1.getAssociatedPoint().getY() > o2.getAssociatedPoint().getY())
             return -1;
-        return Double.compare(o1.getX(), o2.getX());
+        return Double.compare(o1.getAssociatedPoint().getX(), o2.getAssociatedPoint().getX());
     }
 }
